d37881173c6ac35e7972928e7aedd3be9e9c69b4
Michael Reynolds <kmedrang@gmail.com>
2022-05-18 20:04:48 -0700

    Add Components/Update CSS

--- src/components/index-components/Column.tsx
+++ src/components/index-components/Column.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { SubHeader } from './Column-subheader';
 import { ProfileCard } from './Profile';
+import avatar from '../../images/avatar.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard, faBell, faCediSign, faCog, faCompass, faGift, faHeart, faMap, faMessage, faPerson, faPersonBooth, faPersonCirclePlus, faSearch, faSquarePersonConfined } from '@fortawesome/free-solid-svg-icons';
+
+const fontAStyle={
+  //background: 'linear-gradient(90deg, rgba(9,0,156,1) 0%, rgba(4,4,198,1) 45%, rgba(0,212,255,1) 100%)',
+  color: 'aliceblue', 
+  fontSize : '25px',
+  width: '25px',
+  height: '25px',
+  padding: '5px',
+  //backgroundColor: 'rgba(155,155,155,0.5)',
+  backgroundColor: 'black',
+  borderRadius: '20%'
+}
 
 export interface IColumn1Props {}
 export function Column1 (props: IColumn1Props) {
   return (
     <div className="column" id="col-1">
-        <ProfileCard pfp={'./images/woodenPanel1.png'} username={'test'}/>
+        <ProfileCard pfp={avatar} username={'Density'}/>
 
-        <button type="button" className="LeftPanel">Profile</button>
-        <button type="button" className="LeftPanel">Explore</button>
-        <button type="button" className="LeftPanel">Notifications</button>
-        <button type="button" className="LeftPanel">Messages</button>
-        <button type="button" className="LeftPanel">Favorites</button>
-        <button type="button" className="LeftPanel">Compass</button>
-        <button type="button" id="altButton" className="LeftPanel">Settings</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faAddressCard}/> Profile</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faSearch}/> Explore</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faBell}/> Notifications</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faMessage}/> Messages</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faHeart}/> Favorites</button>
+        <button type="button" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faCompass}/> Compass</button>
+        <button type="button" id="altButton" className="LeftPanel"><FontAwesomeIcon style={fontAStyle} icon={faCog}/> Settings</button>
         <button id="logout" type="button" className="LeftPanel">Logout</button>
     </div>
   );
--- src/components/index-components/Profile.tsx
+++ src/components/index-components/Profile.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import avatar from '../../images/avatar.png';
 
 export interface IProfileCardProps {
     username : string;
@@ -7,12 +6,13 @@ export interface IProfileCardProps {
 }
 
 export function ProfileCard (props: IProfileCardProps) {
-    //props.pfp = props.pfp === "" ? `${avatar}` : props.pfp;
-    
     return (
-        <div id="profile">
-            <img id="profilePic" alt="Profile Pic of this user" unselectable="on" style={{objectFit: 'contain'}} src={props.pfp}/>
-            <h3 id="profileUsername" unselectable="on">{`@${props.username}`}</h3>
-        </div>
+        
+        <a href={`/user/${props.username}`} style={{textDecoration : 'none', color : 'black'}}>
+            <div id="profile">
+                <img id="profilePic" alt="Profile Pic of this user" unselectable="on" style={{objectFit: 'contain'}} src={props.pfp}/>
+                <h3 id="profileUsername" unselectable="on">{`${props.username}`}</h3>
+            </div>
+        </a>
     );
 }
